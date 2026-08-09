4bfed5f5619dceab973f29ed5944ec3d5f0b93ef
M1stuki <785397685@qq.com>
2026-08-09 09:13:22 +0800

    fix: synchronize tray parent sizing with explicit text template

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -228,8 +228,8 @@ namespace LiveCaptionsTranslator
             var menu = new WpfContextMenu
             {
                 FontFamily = new Media.FontFamily("Microsoft YaHei UI"),
-                FontSize = 9.25,
-                MinWidth = 116,
+                FontSize = 7.5,
+                MinWidth = 100,
                 StaysOpen = false,
                 UseLayoutRounding = true,
                 SnapsToDevicePixels = true
